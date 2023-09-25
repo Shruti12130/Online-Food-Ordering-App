33422ae4397648b39a018eb8b91406fe9a835ef4
Shruti12130 <shrutijai01@gmail.com>
2023-09-26 01:00:05 +0530

    Added search and filter feature and fetched live data from Swiggy API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,6 @@ export const CDN_URL =
 
 export const LOGO_URL =    
     "https://cdn.pixabay.com/photo/2017/02/17/17/33/food-2074638_960_720.png";
+
+export const API_URL = 
+    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.5940947&lng=85.1375645&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
